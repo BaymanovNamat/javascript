873ee5b52cdf9a018b891ebd19ebc5e1db1f6db9
Baymanov Namat <namataxvd@gmail.com>
2021-12-22 16:01:04 +0600

    add js

---
+++ script2.js
@@ -0,0 +1,50 @@
+alert("Hello world");
+
+// console.log("Hello world");
+
+// declaration 
+var firstName;
+var age;
+
+// initialize 
+var lastName = "Volkov";
+console.log(lastName);
+
+
+// assignment
+firstName = "Petay";
+console.log(firstName);
+
+var object = "math";
+console.log(object);
+
+var figure = "second";
+console.log(figure);
+
+var sport = "boxing";
+console.log(sport);
+
+var fight = "ufs";
+console.log(fight);
+
+var our_country = "Kyrgyzstan";
+console.log(our_country);
+
+
+// example 1 math
+var potatoes = 5, tomatoes = 3;
+var vegetables = potatoes + tomatoes;
+console.log(vegetables);
+
+var result = vegetables / 2;
+console.log(result);
+
+// example 2 math 
+var text = "test";
+console.log(text);
+
+// example 3 math 
+var a = 4;
+var b = 8;
+var rectangleArea = a * b* (a * a * a / (b * b));
+console.log(rectangleArea);
