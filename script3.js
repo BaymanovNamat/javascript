7488f3afda2c34af2c138f5a5ccb57c9d1ee9e2e
Baymanov Namat <namataxvd@gmail.com>
2021-12-22 16:01:46 +0600

    add js

---
+++ script3.js
@@ -0,0 +1,29 @@
+// my home work
+
+// figure is first
+var a = 3;
+var b = 4;
+var theoryOfPhifogor = a * a + b * b;
+console.log(theoryOfPhifogor);
+var c = theoryOfPhifogor / 5;
+console.log(c);
+
+
+var x = 5, y = 8;
+var z = x * y;
+console.log(z);
+
+var aSecondLaw = 25;
+var mSecondLaw = 34;
+var fSecondLaw = aSecondLaw * mSecondLaw;
+console.log(fSecondLaw);
+
+
+var b = -25, c = 8 * 8, d = 4 * 3 * 5;
+var dDown = 52;
+console.log(b + c * c) - d;
+
+
+var halk = "strong";
+console.log(halk);
+
